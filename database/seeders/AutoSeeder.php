a4dada212b3901bfb91403176fc0dac9eb9a389b
Fatih Kuzu <337920@student.mboutrecht.nl>
2025-05-15 13:59:45 +0200

    autoseeder veranderd

--- database/seeders/AutoSeeder.php
+++ database/seeders/AutoSeeder.php
@@ -12,12 +12,19 @@ class AutoSeeder extends Seeder
         $brands = ['Toyota', 'Ford', 'BMW', 'Mercedes', 'Volkswagen'];
         $models = ['Model A', 'Model B', 'Model C', 'Model D', 'Model E'];
         $fuels = ['electric', 'gasoline'];
+        $usedLicensePlates = []; // Track generated license plates
 
         for ($i = 1; $i <= 10; $i++) {
+            do {
+                $licensePlate = 'XX-' . rand(100, 999) . '-YY';
+            } while (in_array($licensePlate, $usedLicensePlates)); // Ensure uniqueness
+
+            $usedLicensePlates[] = $licensePlate;
+
             Auto::create([
                 'brand' => $brands[array_rand($brands)],
                 'model' => $models[array_rand($models)],
-                'license_plate' => 'XX-' . rand(100, 999) . '-YY',
+                'license_plate' => $licensePlate,
                 'fuel' => $fuels[array_rand($fuels)],
                 'is_active' => (bool)rand(0, 1),
                 'photo' => 'photos/auto' . $i . '.jpg', // Example photo path
